e793e37544ca2a1c8b84c78e845ffcbac8a24c8e
pjione <a41924250@gmail.com>
2023-06-09 00:54:43 +0900

    게시글 저장 구현

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -1,6 +1,9 @@
 package com.blog.controller;
 
 import com.blog.request.PostCreate;
+import com.blog.service.PostService;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -13,9 +16,12 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+    private final PostService postService;
     @PostMapping("/posts")
-    public Map<Object, Object> post(@RequestBody @Validated PostCreate postCreate){
+    public Map<Object, Object> post(@RequestBody @Validated PostCreate request){
+        postService.write(request);
         return Map.of();
     }
 }
