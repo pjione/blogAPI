7d8b89da9c1f46aaa242a312b2e2e606d52b2309
pjione <a41924250@gmail.com>
2023-06-12 05:38:57 +0900

    vue 세팅

---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,26 @@
+<script setup lang = "ts">
+
+
+import {ref} from "vue";
+
+const title = ref("")
+const content = ref("")
+
+
+</script>
+
+<template>
+  <div>
+    <el-input type="text" v-model="title" placeholder="제목을 입력해주세요."/>
+  </div>
+  <div class="mt-2">
+    <el-input type="textarea" v-model="content" rows="15"></el-input>
+  </div>
+  <div class="mt-2">
+    <el-button type="primary" @click="write()">글 등록</el-button>
+  </div>
+
+</template>
+
+<style>
+</style>
